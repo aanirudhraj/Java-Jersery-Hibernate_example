dcd7d1dfa37a874caa6d44b6af9fa630db44e69f
aanirudhraj <anirudhraj123@gmail.com>
2017-03-02 21:22:16 -0500

    first commit

---
+++ src/main/java/com/topjavatutorial/MyResource.java
@@ -0,0 +1,79 @@
+package com.topjavatutorial;
+ 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import com.topjavatutorial.dao.Employee;
+import com.topjavatutorial.dao.EmployeeDAO;
+ 
+ 
+@Path("/employees")
+public class MyResource {
+ 
+    @GET
+    @Produces("application/json")
+    public List<Employee> getEmployee() {
+        EmployeeDAO dao = new EmployeeDAO();
+        List employees = dao.getEmployees();
+        return employees;
+    }
+    
+    @GET
+    @Path("/view/{id}")
+    @Produces("application/json")
+    public Employee viewEmployee(@PathParam("id") int id){
+        EmployeeDAO dao = new EmployeeDAO();
+       Employee employee = dao.viewEmployee(id);
+        return employee;
+    }
+ 
+    
+    @POST
+    @Path("/create")
+    @Consumes("application/json")
+    public Response addEmployee(Employee emp){
+        emp.setName(emp.getName());
+        emp.setAge(emp.getAge());
+                
+        EmployeeDAO dao = new EmployeeDAO();
+        dao.addEmployee(emp);
+        
+        return Response.ok().build();
+    }
+    
+    @PUT
+    @Path("/update/{id}")
+    @Consumes("application/json")
+    public Response updateEmployee(@PathParam("id") int id, Employee emp){
+        EmployeeDAO dao = new EmployeeDAO();
+        int count = dao.updateEmployee(id, emp);
+        if(count==0){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.ok().build();
+    }
+    
+    @DELETE
+    @Path("/delete/{id}")
+    @Consumes("application/json")
+    public Response deleteEmployee(@PathParam("id") int id){
+        EmployeeDAO dao = new EmployeeDAO();
+        int count = dao.deleteEmployee(id);
+        if(count==0){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.ok().build();
+    }
+}
+ 
+ 
+ 
